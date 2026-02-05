48f80ac1c3d2f285a37145a8d9c2d1674a15c123
Daniil773-l <daniilrid@gmail.com>
2026-02-05 20:18:07 +0500

    firest

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -8,7 +8,7 @@ import About from "../pages/public/About.jsx";
 import AuthPage from "../pages/auth/AuthPage.jsx";
 import Prices from "../pages/public/Prices.jsx";
 import Contacts from "../pages/public/Contacts.jsx";
-
+import UserDashboard from "../pages/UserPages/UserDashboard.jsx";
 export default function AppRoutes() {
     return (
         <BrowserRouter>
@@ -21,6 +21,7 @@ export default function AppRoutes() {
                     <Route path="/auth" element={<AuthPage/>}/>
                     <Route path="/prices" element={<Prices/>}/>
                     <Route path="/contacts" element={<Contacts/>}/>
+                    <Route path"/profile" element={<ProtectedRoute role="user"><UserDashboard/></ProtectedRoute>}/>
                 </Route>
             </Routes>
             </AuthProvider>
